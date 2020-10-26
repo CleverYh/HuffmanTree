2de83adc71e34da65a089b9b8ba0ec6eb32514f0
CleverYh <cleveryh@qq.com>
2020-10-26 22:43:30 +0800

    update

--- fileCompress.h
+++ fileCompress.h
@@ -100,16 +100,14 @@ public:
         map<unsigned char, string> m;
         recordWeight(m, root);
         int count = 0;
-        if (antithesesName || DEBUG) {
-            ofstream fout2(antithesesName);
-            for (int i = 0; i < 256; i++) {
-                if (_chars[i]->_weight) {
-                    fout2 << (int)_chars[i]->_ch << " " << m[_chars[i]->_ch] << endl;
-                    count++;
-                }
+        ofstream fout2(antithesesName);
+        for (int i = 0; i < 256; i++) {
+            if (_chars[i]->_weight) {
+                fout2 << (int)_chars[i]->_ch << " " << m[_chars[i]->_ch] << endl;
+                count++;
             }
-            fout2.close();
         }
+        fout2.close();
 
         int index = 0;
         struct FILE_HEAD fileHead;
@@ -153,47 +151,11 @@ public:
         // start writing code
         FILE *foutp = fopen(outfilename, "wb");
         // cout << (int)fileHead.sizeOfw << endl;
-        if (DEBUG) cout << (int)fileHead.alphaVariety << endl << (int)fileHead.lastValidBit << endl;
+        if (DEBUG) cout << (int)fileHead.alphaVariety << endl
+                        << (int)fileHead.lastValidBit << endl;
         fwrite(&fileHead, sizeof(struct FILE_HEAD), 1, foutp);
         fwrite(w, sizeof(struct WEIGHT), fileHead.alphaVariety, foutp);
-        // fclose(foutp);
 
-#if 0
-        unsigned char *str;
-        counter = 0;
-        bitIndex = 0;
-        temp = 0;
-        //assert(fileOut2);
-        ch = fgetc(fileOut2);
-        while (!feof(fileOut2)) {
-            cout << 1;
-            str = (unsigned char *)m[ch].c_str();
-            //把每个字符的哈夫曼编码一个一个过。
-            //如果是字符'0'，就转换为二进制的0
-            //如果是字符'1'，就转换为二进制的1
-            for (int i = 0; str[i]; i++) {
-                if ('0' == str[i]) {
-                    //value为一个字节
-                    //从第1位依次赋值，若大于八位（一个字节）了，就写入文件中
-                    CLR_BYTE(temp, bitIndex);
-                } else {
-                    SET_BYTE(temp, bitIndex);
-                }
-                bitIndex++;
-                if (bitIndex >= 8) {
-                    bitIndex = 0;
-                    fwrite(&temp, sizeof(unsigned char), 1, foutp);
-                }
-            }
-            ch = fgetc(fileOut2);
-        }
-        //如果最后一次不满一个字节，依然需要写到文件中，注意：写入的最后一个字节可能会存在垃圾位
-        if (bitIndex) {
-            fwrite(&temp, sizeof(unsigned char), 1, foutp);
-        }
-#endif
-        // counter = 0;
-        // bitIndex = 0;
         fileOut2 = fopen(filename, "rb");
         temp = 0;
         counter = 0;
@@ -227,23 +189,7 @@ public:
                 counter = 0;
             }
         }
-        // fout.close();
-        // // cout << (int)fileHead.lastValidBit << endl;
-        // while (counter) { // fullfill the last byte
-        //     temp <<= 1;
-        //     counter++;
-        //     if (counter == 8) {
-        //         fout << temp;
-        //         // code.push_back(temp);
-        //         counter = 0; // break;
-        //     }
-        // }
 
-        // unsigned char *c = (unsigned char *)(code.c_str());
-        // fwrite(c, sizeof(unsigned char), code.size(), foutp);
-
-        // fout << code;
-        // fout.close();
         fclose(foutp);
         fclose(fileOut2);
 
